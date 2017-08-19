b3c8d4686387d63c5f24f9f92b8ae11f8727d61d
progand <lach@ukr.net>
2017-08-19 19:06:09 +0300

    [+] EMR radiology pet

--- app/emrRadiology/index.ts
+++ app/emrRadiology/index.ts
@@ -1,8 +1,8 @@
 import { radiologyEMRTypeId } from '../emrTypes/emrTypes';
 import { textProcedureId } from '../templateEMRText/templateEMRText';
 import { ctMotalityId, ptMotalityId } from '../modalities/modalities';
-import {ctTemplateEMRRadiologyId, ptTemplateEMRRadiologyId} from '../templateEMRRadiology/templateEMRRadiology';
-import {mZvUnitId} from '../units/units';
+import { ctTemplateEMRRadiologyId, ptTemplateEMRRadiologyId } from '../templateEMRRadiology/templateEMRRadiology';
+import { mZvUnitId } from '../units/units';
 
 export async function removeEMRRadiology(conn: any): Promise<void> {
     console.log(`Deleting all data emrRadiology...`);
@@ -18,13 +18,37 @@ export async function removeEMRRadiology(conn: any): Promise<void> {
 }
 
 export async function importEMRRadiology(conn: any): Promise<void> {
-    let query = '';
     console.log(`Importing emrRadiology...`);
 
     try {
         await conn.query('alter table avalon.general_emrgeneral add column import_id int;');
     } catch (e) { }
-    //import CT records
+
+    await importRadiology(conn, 'CT');
+    await importRadiology(conn, 'PT');
+
+    console.log(`emrRadiology import finished.`);
+    return conn;
+}
+
+async function importRadiology(conn: any, modality: String): Promise<void> {
+    let query: String = '',
+        templateEMRRadiologyId: Number = 0,
+        motalityId: Number = 0,
+        unitId: Number = mZvUnitId,
+        tableDetails: String = '',
+        queryResult: any = { message: '' };
+
+    if (modality === 'CT') {
+        templateEMRRadiologyId = ctTemplateEMRRadiologyId;
+        motalityId = ctMotalityId;
+        tableDetails = 'ct';
+    } else if (modality === 'PT') {
+        templateEMRRadiologyId = ptTemplateEMRRadiologyId;
+        motalityId = ptMotalityId;
+        tableDetails = 'pet';
+    }
+    //import records
     query = `INSERT INTO avalon.general_emrgeneral 
         (id, create_date, update_date, patient_id, creator_id, procedure_id, emr_package_id, import_id)
         SELECT 
@@ -33,7 +57,7 @@ export async function importEMRRadiology(conn: any): Promise<void> {
             coalesce(r.study_date_time, now()),
             u.id,
             (select id from avalon.account_userprofile where username='admin' limit 1),
-            ${ctTemplateEMRRadiologyId},    
+            ${templateEMRRadiologyId},    
             p.id,
             r.id
         FROM
@@ -44,33 +68,34 @@ export async function importEMRRadiology(conn: any): Promise<void> {
             avalon.general_emrpackage p on r.id=p.import_id
         WHERE
             r.check_del != 'yes' and r.r_ct='так'`;
-    console.log(` --- general EMR CT...`);
-    await conn.query(query);
+    console.log(` --- general EMR ${modality}...`);
+    queryResult = await conn.query(query);
+    console.log(queryResult.message);
 
     query = `INSERT INTO avalon.radiology_emrradiology 
-    (create_date, update_date, study_number, dose, study_date, emr_general_id, localization_id, modality_id, unit_id)
-    SELECT 
+        (create_date, update_date, study_number, dose, study_date, emr_general_id, localization_id, modality_id, unit_id)
+        SELECT 
         COALESCE(r.study_date_time, NOW()) create_date,
         COALESCE(r.study_date_time, NOW()) update_date,
         r.study_id study_number,
-        ct.ct_dose,
+        ctorpet.${tableDetails}_dose,
         COALESCE(r.study_date_time, NOW()) study_date,
         g.id emr_general_id,
         null localization_id,
-        ${ctMotalityId} modality_id,
-        ${mZvUnitId} unit_id
-    FROM
+        ${motalityId} modality_id,
+        ${unitId} unit_id
+        FROM
         cliniccore.research r
             LEFT JOIN
         avalon.general_emrgeneral g ON r.id = g.import_id
             LEFT JOIN
-        cliniccore.ct ct ON r.id_ct = ct.id
-    WHERE
+        cliniccore.${tableDetails} ctorpet ON r.id_${tableDetails} = ctorpet.id
+        WHERE
         r.check_del != 'yes'
-            AND g.procedure_id = ${ctTemplateEMRRadiologyId}`;
-    console.log(` --- radiology EMR CT...`);
-    console.log(await conn.query(query));
+            AND g.procedure_id = ${templateEMRRadiologyId}`;
+    console.log(` --- radiology EMR ${modality}...`);
+    queryResult = await conn.query(query);
+    console.log(queryResult.message);
 
-    console.log(`emrRadiology import finished.`);
     return conn;
 }
